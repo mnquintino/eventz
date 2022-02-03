763adac09bcb9c49b6b1a04a78187cf9f4fc41b8
Manara <mnquintino@gmail.com>
2022-02-03 10:57:10 -0300

    13, 14, 15 - Form Create, Parcials, Destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,16 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
   def show
     @event = Event.find(params[:id])
   end
@@ -14,10 +24,21 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    event_params = 
-      params.require(:event).
-        permit(:name, :description, :location, :price, :starts_at)
     @event.update(event_params)
     redirect_to @event
   end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_url
+  end
+
+private
+
+  def event_params
+    params.require(:event).
+      permit(:name, :description, :location, :price, :starts_at)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
   root "events#index"
+
+  resources :events
   
-  get "events" => "events#index"
-  get "events/:id" => "events#show", as:"event"
-  get "events/:id/edit" => "events#edit", as:  "edit_event"
-  patch "events/:id" => "events#update"
+  # get "events" => "events#index"
+  # get "events/new" => "events#new"
+  # get "events/:id" => "events#show", as:"event"
+  # get "events/:id/edit" => "events#edit", as:  "edit_event"
+  # patch "events/:id" => "events#update"
+
 end
