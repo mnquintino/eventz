ce4e0eea594d813437d6842cf1492a687a912c01
Manara <mnquintino@gmail.com>
2022-02-09 10:34:15 -0300

    29-32.Sign in/up/out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user_id
       redirect_to @user,
       notice: "Thanks for signing up!"
     else
@@ -38,6 +39,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to events_url, alert: "Account successfully deleted!"
   end
 
