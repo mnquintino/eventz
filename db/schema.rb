7c9c2b1076c3d1fe8f552ceb1e419449c1053f06
Manara <mnquintino@gmail.com>
2022-02-09 17:19:23 -0300

    33-34.Authorization; admin users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_140325) do
+ActiveRecord::Schema.define(version: 2022_02_09_174923) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2022_02_08_140325) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "registrations", "events"
